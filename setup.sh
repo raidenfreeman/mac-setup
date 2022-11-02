64b2d18fe7fc98c6f0f17185d401f75b35605cdf
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2022-11-02 23:22:58 +0100

    Streamline bash setup script
    Don't show output unless necessary
    Don't install empty roles
    Don't use file for a single value for collections

--- setup.sh
+++ setup.sh
@@ -1,58 +1,48 @@
 #!/bin/bash
 
-function install_xcode_commandline_tools() {
-    echo -n "Installing XCode commandline tools : "
+set -euo pipefail
 
-    xcode-select -p 1>/dev/null
-    xcode_installed=$?
-    if [ $xcode_installed -eq 2 ];
-    then
-        echo "Installing xcode tools"
-        xcode-select --install
-        echo "[ Installed ]"
-    else
-        echo "[ Skipping ]"
-    fi
+function installXcodeCLI() {
+  xcode-select -p 1>/dev/null
+  isXcodeInstalled=$?
+  if [ $isXcodeInstalled -eq 2 ];
+  then
+    echo "Installing xcode tools..."
+    xcode-select --install
+    echo "== Done =="
+  fi
 }
 
-function install_brew() {
-    echo -n "Installing brew :                  "
-    if ! command -v brew &> /dev/null
-    then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo "[ Installed ]"
-    else
-        echo "[ Skipping ]"
-    fi
+function installHomebrew() {
+  if ! command -v brew &> /dev/null
+  then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "== Done =="
+  fi
 }
 
-function install_ansible() {
-    echo -n "Installing ansible :               "
-
-    if ! command -v ansible &> /dev/null
-    then
-        brew install ansible
-        echo "[ Installed ]"
-    else
-        echo "[ Skipping ]"
-    fi
+function installAnsible() {
+  if ! command -v ansible &> /dev/null
+  then
+    echo "Installing Ansible..."
+    brew install ansible
+    echo "== Done =="
+  fi
 }
 
 echo "PATH="$PATH
 export PATH=$PATH:/opt/homebrew/bin
 
-install_xcode_commandline_tools
-install_brew
-install_ansible
+installXcodeCLI
+installHomebrew
+installAnsible
 
 SCRIPT_DIR=$(dirname "$0")
 pushd $SCRIPT_DIR
 
-echo "Installing galaxy roles"
-ansible-galaxy role install -r requirements.yml
-
 echo "Installing galaxy collections"
-ansible-galaxy collection install -r requirements.yml
+ansible-galaxy collection install community.general
 
 echo "Installing tools"
 sudo -n true
